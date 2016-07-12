3053921763962536431df74d42a6e96f84fb0aa6
Nathan Houle <nathan@nathanhoule.com>
2016-07-12 14:13:59 -0700

    Modernize test harness; remove Duo support, add Browserify support

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,10 @@
+'use strict';
 
-var Analytics = require('analytics.js-core').constructor;
-var integration = require('analytics.js-integration');
-var sandbox = require('clear-env');
-var tester = require('analytics.js-integration-tester');
-var Wishpond = require('../lib/');
+var Analytics = require('@segment/analytics.js-core').constructor;
+var integration = require('@segment/analytics.js-integration');
+var sandbox = require('@segment/clear-env');
+var tester = require('@segment/analytics.js-integration-tester');
+var Wishpond = require('../lib');
 
 describe('Wishpond', function() {
   var analytics;
